5f9b1b0e215ce14c531a96da36575fa34ca5faef
Lara García Carnés <laragarciacarnes@MacBook-Air-de-Lara.local>
2023-12-02 21:31:15 +0100

    initial commit

---
+++ app/src/main/java/com/example/astralhub/ui/luck/LuckFragment.kt
@@ -0,0 +1,28 @@
+package com.example.astralhub.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.astralhub.R
+import com.example.astralhub.databinding.FragmentHoroscopeBinding
+import com.example.astralhub.databinding.FragmentLuckBinding
+
+
+class LuckFragment : Fragment() {
+
+    private var _binding: FragmentLuckBinding?=null
+    private val binding get()=_binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding= FragmentLuckBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+
+}
